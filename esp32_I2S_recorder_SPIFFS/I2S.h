39be96e22cec861ceb535e8bf817863c27529218
lig@nt <jimkaragian@yahoo.gr>
2020-05-03 20:48:10 +0300

    added SPIFFS support for esp32
    with SPIFFS there is no need for SD card

---
+++ esp32_I2S_recorder_SPIFFS/I2S.h
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2s.h"
+#include "esp_system.h"
+#define SAMPLE_RATE (44100)
+#define PIN_I2S_BCLK 26
+#define PIN_I2S_LRC 22
+#define PIN_I2S_DIN 34
+#define PIN_I2S_DOUT 25
+
+// This I2S specification : 
+//  -   LRC high is channel 2 (right).
+//  -   LRC signal transitions once each word.
+//  -   DATA is valid on the CLOCK rising edge.
+//  -   Data bits are MSB first.
+//  -   DATA bits are left-aligned with respect to LRC edge.
+//  -   DATA bits are right-shifted by one with respect to LRC edges.
+//        It's valid for ADMP441 (microphone) and MAX98357A (speaker). 
+//        It's not valid for SPH0645LM4H(microphone) and WM8960(microphon/speaker).
+//
+//  -   44100Hz
+//  -   stereo
+
+/// @parameter MODE : I2S_MODE_RX or I2S_MODE_TX
+/// @parameter BPS : I2S_BITS_PER_SAMPLE_16BIT or I2S_BITS_PER_SAMPLE_32BIT
+void I2S_Init(i2s_mode_t MODE, i2s_bits_per_sample_t BPS);
+
+/// I2S_Read() for I2S_MODE_RX
+/// @parameter data: pointer to buffer
+/// @parameter numData: buffer size
+/// @return Number of bytes read
+int I2S_Read(char* data, int numData);
+
+/// I2S_Write() for I2S_MODE_TX
+/// @param data: pointer to buffer
+/// @param numData: buffer size
+void I2S_Write(char* data, int numData);
---
+++ esp32_I2S_recorder_SPIFFS/esp32_I2S_recorder_SPIFFS.ino
@@ -0,0 +1,87 @@
+#include "Arduino.h"
+#include <FS.h>
+#include "Wav.h"
+#include "I2S.h"
+#include "SPIFFS.h"
+#define FORMAT_SPIFFS_IF_FAILED true
+
+//comment the first line and uncomment the second if you use MAX9814
+//#define I2S_MODE I2S_MODE_RX
+#define I2S_MODE I2S_MODE_ADC_BUILT_IN
+
+const int record_time = 5;  // second
+const char filename[] = "/sound.wav";
+
+const int headerSize = 44;
+const int waveDataSize = record_time * 88000;
+const int numCommunicationData = 8000;
+const int numPartWavData = numCommunicationData/4;
+byte header[headerSize];
+char communicationData[numCommunicationData];
+char partWavData[numPartWavData];
+File file;
+
+
+void listDir(fs::FS& fs, const char* dirname, uint8_t levels) {
+  // Serial.printf("Listing directory: %s\r\n", dirname);
+
+  File root = fs.open(dirname);
+  if (!root) {
+    // Serial.println("- failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    // Serial.println(" - not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if (levels) {
+        listDir(fs, file.name(), levels - 1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("\tSIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+    Serial.println("SPIFFS Mount Failed");
+    return;
+  }
+  // https://gitter.im/espressif/arduino-esp32?at=5dfeb2170cb24d1d44fd6d1c
+  SPIFFS.format();
+  CreateWavHeader(header, waveDataSize);
+  SPIFFS.remove(filename);
+  file = SPIFFS.open(filename, FILE_WRITE);
+  if (!file) return;
+  Serial.println("start recording");
+  file.write(header, headerSize);
+  I2S_Init(I2S_MODE, I2S_BITS_PER_SAMPLE_32BIT);
+  for (int j = 0; j < waveDataSize/numPartWavData; ++j) {
+    I2S_Read(communicationData, numCommunicationData);
+    for (int i = 0; i < numCommunicationData/8; ++i) {
+      partWavData[2*i] = communicationData[8*i + 2];
+      partWavData[2*i + 1] = communicationData[8*i + 3];
+    }
+    file.write((const byte*)partWavData, numPartWavData);
+  }
+  file.close();
+  Serial.println("finish recording");
+  Serial.println("test SPIFFS recording file");
+  listDir(SPIFFS, "/", 0);
+  
+}
+
+void loop() {
+}
